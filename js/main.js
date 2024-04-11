d90ddd5448e1ecf24d4b436417db91e605c0eebc
Bas-jpg <302806549@student.rocmondriaan.nl>
2024-04-11 10:20:16 +0200

    bootstrapifide my code and made it one page

--- js/main.js
+++ js/main.js
@@ -2,3 +2,11 @@ const logo = document.querySelector(".logo");
 logo.addEventListener("click", function() {
     window.location.href = "index.html";
 });
+
+// const abBtn = document.querySelector(".about-btn");
+// abBtn.addEventListener('click', () => {
+//     abBtn.classList.add("transitioning");
+//     setTimeout(() => {
+//         window.location.href = 'about.html';
+//     }, 5000);
+//  });
