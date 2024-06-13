6ed65c99a856a77643371a79c2070db582eab8d5
Bas-jpg <302806549@student.rocmondriaan.nl>
2024-06-13 09:38:08 +0200

    oke nu is de bug gefixed
    bug is nu gefixed

--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
 // deze button rederect je naar de about section van de pagina
 const aboutBtn = document.querySelector(".about-btn");
 aboutBtn.addEventListener('click', () => {
-    window.location.replace('index.html#about-me')
+    window.location.replace('bas-jpg.github.io#about-me')
  });
 
  // deze button rederect je naar de contact section van de pagina
  const contactBtn = document.querySelector(".contact-btn");
  contactBtn.addEventListener('click', () => {
-    window.location.replace('index.html#contact')
+    window.location.replace('bas-jpg.github.io#contact')
  });
  
 // deze button rederect je naar de project section van de pagina
  const projectBtn = document.querySelector(".project-btn");
  projectBtn.addEventListener('click', () => {
-    window.location.replace('index.html#projecten')
+    window.location.replace('bas-jpg.github.io#projecten')
  });
 
 // deze button rederect je naar de portfolio section van de pagina
  const portfolioBtn = document.querySelector(".portfolio-btn");
  portfolioBtn.addEventListener('click', () => {
-    window.location.replace('index.html#about-portfolio')
+    window.location.replace('bas-jpg.github.io#about-portfolio')
  });
 
 
