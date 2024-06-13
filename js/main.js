5e961f7b47d3a4bd74c711b3c20be55de0e17882
Bas-jpg <302806549@student.rocmondriaan.nl>
2024-06-13 09:37:17 +0200

    contact af
    contact is af, heb een bug in de sidebar gefixed en ik ben nu bezig met de about portfolio

--- js/main.js
+++ js/main.js
@@ -1,23 +1,35 @@
 // deze button rederect je naar de about section van de pagina
 const aboutBtn = document.querySelector(".about-btn");
 aboutBtn.addEventListener('click', () => {
-    window.location.replace('http://127.0.0.1:5500/index.html#about-me')
+    window.location.replace('index.html#about-me')
  });
 
  // deze button rederect je naar de contact section van de pagina
  const contactBtn = document.querySelector(".contact-btn");
  contactBtn.addEventListener('click', () => {
-    window.location.replace('http://127.0.0.1:5500/index.html#contact')
+    window.location.replace('index.html#contact')
  });
  
 // deze button rederect je naar de project section van de pagina
  const projectBtn = document.querySelector(".project-btn");
  projectBtn.addEventListener('click', () => {
-    window.location.replace('http://127.0.0.1:5500/index.html#projecten')
+    window.location.replace('index.html#projecten')
  });
 
 // deze button rederect je naar de portfolio section van de pagina
  const portfolioBtn = document.querySelector(".portfolio-btn");
  portfolioBtn.addEventListener('click', () => {
-    window.location.replace('http://127.0.0.1:5500/index.html#about-portfolio')
+    window.location.replace('index.html#about-portfolio')
  });
+
+
+
+//  pakt de paragraf
+ const copyText = document.querySelector(".email");
+ copyText.addEventListener('click', () => {
+    // copied deze tekst en zet het op jouw klipboard
+    navigator.clipboard.writeText('bdvandenberg@icloud.com');
+    
+    // alert dat de text is gekopied
+    alert("Copied the email!");
+});
